6435abc7efbce453fe268db741f48973283adbf5
Lott-Campuzano <lottocampuzano@gmail.com>
2024-02-09 01:37:47 -0600

    Por fin se creo el bendito proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header.jsx";
+
+function App() {
+
+  return (
+    <>
+      <header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <h1 className="text-4xl">
+            Hola mundo
+        </h1>
+    )
+}
+export default Header
+
+
